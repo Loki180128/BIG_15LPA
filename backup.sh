408160e59807920c8577a11050ce3dababca0d63
AniketJadhav1 <jadhavaniket2807@gmail.com>
2025-07-02 22:56:28 +0000

    Added a small change

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@
 
 this shell scripts will take backups
 
+can also be used with crown
 info
 
 src=$1
@@ -15,3 +16,4 @@ echo -r "$dest/backup-$timestamp.zip" $src > /dev/null
 
 echo "backup done"
  
+echo "Kya haal hai"
